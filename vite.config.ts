bc704c0f9ba31db8288cb203b30a0ff6b6948553
Patrick Miller <pamiller.pdx@gmail.com>
2024-08-04 01:53:22 +0900

    update remix and misc versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import {
 	vitePlugin as remix,
 	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import AutoImport from 'unplugin-auto-import/vite';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+} from "@remix-run/dev";
+import AutoImport from "unplugin-auto-import/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
 		AutoImport({
-			imports: ['react'],
+			imports: ["react"],
 			biomelintrc: {
 				enabled: true,
 			},
@@ -20,6 +20,8 @@ export default defineConfig({
 				v3_fetcherPersist: true,
 				v3_relativeSplatPath: true,
 				v3_throwAbortReason: true,
+				unstable_singleFetch: true,
+				unstable_lazyRouteDiscovery: true,
 			},
 		}),
 		tsconfigPaths(),
